d292171745a18785c01a92cec3152e9001f3c26a
aakare <45142477+aakare@users.noreply.github.com>
2019-04-28 20:32:05 +0200

    add dNd support

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
@@ -10,12 +11,13 @@ namespace He2
 {
     class Program
     {
+        static bool operate = true;
+
+        static List<Plugin> pList = new List<Plugin>();
         static void Main(string[] args)
         {
 
             System.IO.Directory.CreateDirectory(@"C:\He2");
-
-            List<Plugin> pList = new List<Plugin>();
 
             string[] plugins = System.IO.Directory.GetFiles(@"C:\He2", "*.dll");
             foreach (string plugin in plugins)
@@ -36,60 +38,78 @@ namespace He2
                     }
                 }
             }
-            bool operate = true;
             Console.Title = "He2";
             Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine();
-            while (operate)
+            if (args.Length > 0)
             {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("~" + System.Security.Principal.WindowsIdentity.GetCurrent().Name);
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.Write("$ ");
-                Console.ForegroundColor = ConsoleColor.White;
-                string i;
-                i = Console.ReadLine();
-                string[] il = i.Split(' ');
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                bool pluginExed = false;
-                foreach (Plugin plugin in pList)
+                StreamReader sr = new StreamReader(args[0]);
+                string[] commands = sr.ReadToEnd().Split('\n');
+
+                foreach (string command in commands)
                 {
-                    if (il[0].ToLower() == "./" + plugin.Name().ToLower())
-                    {
-                        plugin.Code(il.Skip(1).ToArray());
-                        pluginExed = true;
-                        break;
-                    }
+                    if(command.Length > 0)
+                        execCommand(command);
                 }
-                if (il[0] == "stop")
-                    operate = false;
-                else if (il[0] == "clt")
-                    Console.Clear();
-                else if (il[0] == "?" || il[0] == "wonders")
-                    if (il[1] == "")
-                    {
-                        Console.WriteLine("He2 Wonders");
-                        Console.WriteLine("? - help | arguments: --l - plugin list");
-                        Console.WriteLine("");
-                    }
-                    else
+            }
+            else
+                while (operate)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("~" + System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write("$ ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    string i;
+                    i = Console.ReadLine();
+                    execCommand(i);
+                }
+        }
+
+        public static void execCommand(string i)
+        {
+            string[] il = i.Split(' ');
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            bool pluginExed = false;
+            foreach (Plugin plugin in pList)
+            {
+                if (il[0].ToLower() == "./" + plugin.Name().ToLower())
+                {
+                    plugin.Code(il.Skip(1).ToArray());
+                    pluginExed = true;
+                    break;
+                }
+            }
+            if (il[0] == "stop")
+                operate = false;
+            else if (il[0] == "clt")
+                Console.Clear();
+            else if (il[0] == "##end")
+                Console.ReadKey();
+            else if (il[0] == "?" || il[0] == "wonders")
+                if (il[1] == "")
+                {
+                    Console.WriteLine("He2 Wonders");
+                    Console.WriteLine("? - help | arguments: --l - plugin list");
+                    Console.WriteLine("");
+                }
+                else
                     foreach (string arg in il)
                     {
                         if (arg == "--l")
                         {
                             Console.WriteLine("HePlug | Plugin list");
-                                Console.WriteLine();
-                            foreach(Plugin plugin in pList)
-                                {
-                                    Console.WriteLine("Name > " + plugin.Name() + " > Description > " + plugin.Description() + " > Help > " + plugin.Help());
-                                }
+                            Console.WriteLine();
+                            foreach (Plugin plugin in pList)
+                            {
+                                Console.WriteLine("Name > " + plugin.Name() + " > Description > " + plugin.Description() + " > Help > " + plugin.Help());
+                            }
                         }
                     }
 
-                else
-                    if(!pluginExed)
-                        Console.WriteLine("~if: command not found, \"?\" for help.");
-            }
+            else
+                if (!pluginExed)
+                Console.WriteLine("~if: command not found, \"?\" for help.");
         }
-    }
+        }
 }
